59134b6e769b737df0eeecdd90341316e5f81243
lhassler <lhassler@SEW-01489-LHH>
2018-10-05 14:54:16 +0200

    added timer

--- GitProject/src/gitproject/Tunnel.java
+++ GitProject/src/gitproject/Tunnel.java
@@ -8,7 +8,9 @@ package gitproject;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,25 +18,28 @@ import javax.swing.JPanel;
  *
  * @author lhassler
  */
-public class Tunnel extends JPanel implements MouseMotionListener {
+public class Tunnel extends JPanel implements MouseListener {
     private int mouseX, mouseY = 0;
-    
+    int spin = 0;
+
     public Tunnel(int width, int height) {
         this.setBackground(Color.white);
         this.setSize(width, height);
-    }
-    
-    @Override
-    public void mouseDragged(MouseEvent me) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void mouseMoved(MouseEvent me) {
-        mouseX = me.getX()-10;
-        mouseY = me.getY()-30;
-        repaint();
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+            @Override
+            public void run() {
+              if (spin<90)
+              {
+                  spin += 1;
+              }
+              else
+              {
+                  spin = 0;
+              }
+            }
+        }, 20, 20);
     }
     
     @Override
@@ -42,28 +47,25 @@ public class Tunnel extends JPanel implements MouseMotionListener {
     {
         g.setColor(Color.white);
         g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        g.setColor(Color.green);
+        g.drawLine(mouseX, mouseY, mouseX+50, mouseY+300);
         g.setColor(Color.black);
-        for (int i=0; i<20; i++)
+        for (int i=0; i<360; i++)
         {
-            g.drawLine(i*40, 0, mouseX, mouseY);
-        }
-        for (int i=0; i<20; i++)
-        {
-            g.drawLine(i*40, 600, mouseX, mouseY);
-        }
-        for (int i=0; i<20; i++)
-        {
-            g.drawLine(0, i*30, mouseX, mouseY);
-        }
-        for (int i=0; i<21; i++)
-        {
-            g.drawLine(800, i*30, mouseX, mouseY);
+            if(i%10 == 0)
+            {
+                g.drawLine(mouseX, mouseY, mouseX+(int)(Math.sin(Math.toRadians(i+spin))*100), mouseY+(int)(Math.cos(Math.toRadians(i+spin))*100));
+                g.setColor(Color.white);
+                g.fillOval(mouseX+(int)(Math.sin(Math.toRadians(i+spin))*100)-5, mouseY+(int)(Math.cos(Math.toRadians(i+spin))*100)-5, 10, 10);
+                g.setColor(Color.black);
+                g.drawOval(mouseX+(int)(Math.sin(Math.toRadians(i+spin))*100)-5, mouseY+(int)(Math.cos(Math.toRadians(i+spin))*100)-5, 10, 10);
+            }   
         }
         g.setColor(Color.white);
-        g.fillOval(mouseX-100, mouseY-100, 200, 200);
+        g.fillOval(mouseX-20, mouseY-20, 40, 40);
         g.setColor(Color.black);
-        g.drawOval(mouseX-100, mouseY-100, 200, 200);
-        
+        g.drawOval(mouseX-20, mouseY-20, 40, 40);
+        repaint();
     }
 
     
@@ -77,9 +79,34 @@ public class Tunnel extends JPanel implements MouseMotionListener {
         frame.setLayout(null); 
         Tunnel tPanel = new Tunnel(frame.getWidth(),frame.getHeight());
         frame.add(tPanel);
-        frame.addMouseMotionListener(tPanel);
+        frame.addMouseListener(tPanel);
     }
-    
-    
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+        mouseX = me.getX()-5;
+        mouseY = me.getY()-25;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
